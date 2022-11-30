11c375ce9ecee0b238359fd58b4fbba7dfb71e2f
Pat Ging <patrickging04@gmail.com>
2022-11-29 19:00:56 -0500

    implementation

--- main.cpp
+++ main.cpp
@@ -8,15 +8,29 @@
 #include "myvector.h"
 
 int main() {
-	std::cout
+	std::cout << "Initializing array" << std::endl;
 	MyVector mv;
 	mv.push_back(5);
 	mv.push_back(15);
 	mv.push_back(225);
+	mv.push_back(25);
+	mv.push_back(22);
 
+	std::cout << "Pushing in 5, 15, 225,25,22" << std::endl;
+	std::cout << mv.to_string() << std::endl;
+	std::cout << "Size is " << mv.size();
+
+	mv.pop_back();
+	std::cout << "Popping the first item" << std::endl;
 	std::cout << mv.to_string() << std::endl;
 
-	mv.empty();
-	std::cout << mv.to_string();
+	mv.pop_back(3);
+	std::cout << "Popping 3 items" << std::endl;
+	std::cout << mv.to_string() << std::endl;
+
+	mv.clear();
+	std::cout << "Clear items" << std::endl;
+	std::cout << mv.to_string() << std::endl;	
+
 	return 0;
 }
